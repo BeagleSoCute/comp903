6d32b0c186ed47a5f77bf2f645622af8eb428909
TanawatMfec <tanawat_l@mfec.co.th>
2024-03-29 21:55:22 +1300

    push

--- src/App.jsx
+++ src/App.jsx
@@ -6,145 +6,44 @@ import SortingSection from "./child-components/SortingSection";
 import ShowTotalPrice from "./child-components/ShowTotalPrice";
 import Button from "./share-components/Button";
 import PurchaseHistoryLogs from "./child-components/PurchaseHistoryLogs";
-import { generateProduct, lightweightGenProduct } from "./services/product.service";
+import {
+  generateProduct,
+  lightweightGenProduct,
+} from "./services/product.service";
 import { generateExpensiveComputationalFunc } from "./services/common.service";
 
+// Child Component
+const ChildComponent = React.memo(({onClick }) => {
+  // const handleOnClick = () => {
+  //   onClick((pre) => pre+1);
+  // }
+
+  console.log("Child Component Rendered");
+  return <button onClick={onClick}>Click Me</button>;
+});
+
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [defaultProduct, setDefaultProducts] = useState([]);
-  const [sortOrder, setSortOrder] = useState(null);
-  const [currentTab, setCurrentTab] = useState("productPage");
-  const [historyLogs, setHistoryLogs] = useState([]);
-  const [intervalId, setIntervalId] = useState(null);
-  const [displayForm, setDisplayForm] = useState(false);
+  const [count, setCount] = useState(0);
 
-  const generateProducts = useCallback(() => {
-    const thisProducts = [];
-    for (let i = 0; i < 80000; i++) {
-      thisProducts.push(lightweightGenProduct());
-    }
-    setDefaultProducts([...products, ...thisProducts]);
-    setProducts([...products, ...thisProducts]);
-  }, [products]);
-  const handleSort = (type) => {
-    setSortOrder(type);
-  };
+  // Define callback function within Parent Component
+  const handleClick = useCallback(() => {
+    setCount((pre) => pre+1);
+    // console.log('count call')
+  }, []);
 
-  const handleTotalPrice = () => {
-    // generateExpensiveComputationalFunc();
-    let sum = 0;
-    for (let i = 0; i <= products.length; i++) {
-      if (products[i]) {
-        const result = products[i].price + sum;
-        sum = result;
-      }
-    }
-    return sum;
-  };
-
-  const handleSortedProducts = () => {
-    // generateExpensiveComputationalFunc();
-    if (!sortOrder) return products;
-    return [...products].sort((a, b) => {
-      const nameA = a.name.toUpperCase();
-      const nameB = b.name.toUpperCase();
-      return sortOrder === "asc"
-        ? nameA < nameB
-          ? -1
-          : 1
-        : nameA > nameB
-        ? -1
-        : 1;
-    });
-  };
-
-  const sortedProducts = handleSortedProducts();
-  const totalPrice = handleTotalPrice();
-  const handleAddProduct = (newProduct) => {
-    setDefaultProducts([newProduct, ...products]);
-    setProducts([newProduct, ...products]);
-  };
-
-  const handleDeleteAllProduct = () => {
-    setProducts([]);
-  };
-
-  const handleClearSort = () => {
-    setSortOrder(null);
-    setProducts(defaultProduct);
-  };
-  const handleOpenForm = (value) => {
-    setDisplayForm(value);
-  };
-
-  const startGeneratingHistoryLogs = () => {
-    const id = setInterval(() => {
-      const newProduct = generateProduct();
-      setHistoryLogs((prevProducts) => [...prevProducts, newProduct]);
-    }, 1000);
-    setIntervalId(id);
-  };
-  const stopGeneratingHistoryLogs = () => {
-    clearInterval(intervalId);
-    setIntervalId(null);
-  };
-  const handleDeleteProduct = useCallback((id) => {
-    const removedProduct = products.filter((item) => item.id !== id);
-    setProducts(removedProduct);
-  },[products])
-
- //-----------------Test third hypothesis---------------------------------------------------------------------- 
-  // const handleShowMessage = useCallback((id) => {
-  //   console.log('handleShowMessage',setSortOrder)
-  // },[sortOrder])
+  console.log("Parent Component Rendered");
 
   return (
-    <div className="app">
-      <Menu />
-      <div className="content-wrapper">
-        <div>
-          <div className="flex justify-center mt-3 mb-6">
-            <Button onClick={() => setCurrentTab("productPage")}>
-              Product page
-            </Button>
-            <Button onClick={() => setCurrentTab("historyPage")}>
-              History page
-            </Button>
-          </div>
-          {currentTab === "productPage" ? (
-            <>
-              <div className="flex justify-between mb-5">
-                <SortingSection
-                  sortOrder={sortOrder}
-                  onChange={handleSort}
-                  onClear={handleClearSort}
-                />
-                <p>The number of products: {products.length}</p>
-                <ShowTotalPrice totalPrice={totalPrice} />
-              </div>
-              <AddingProductSection
-                displayForm={displayForm}
-                onAdd={handleAddProduct}
-                onRandomAdd={generateProducts}
-                onDisplayForm={handleOpenForm}
-              />
-              <ProductLists
-                products={sortedProducts}
-                onDelete={handleDeleteProduct}
-                onDeleteAll={handleDeleteAllProduct}
-                // onShowMessage={handleShowMessage}
-                // onShowMessage={() => {}}
-              />
-            </>
-          ) : (
-            <PurchaseHistoryLogs
-              logs={historyLogs}
-              onStart={startGeneratingHistoryLogs}
-              onStop={stopGeneratingHistoryLogs}
-            />
-          )}
-        </div>
-      </div>
+    <div>
+      <h1>Parent Component</h1>
+      <p>Count: {count}</p>
+      {/* Render multiple Child Components */}
+      {[...Array(10000)].map((_, index) => (
+        <ChildComponent key={index} 
+        // onClick={setCount}
+        onClick={handleClick}
+         />
+      ))}
     </div>
   );
 };
